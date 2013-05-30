cca67aff556c0ba553aa859856a584cb3b6783b1
David Watson <david@bashton.com>
2013-05-30 10:20:01 +0000

    Add imap-server.js file for use on the server.

--- package.js
+++ package.js
@@ -6,3 +6,7 @@ Npm.depends({
   "utf7": "1.0.0",
   "imap": 'https://github.com/BashtonLtd/node-imap/tarball/0a8a4d5294f6c3291ddc96efc84c88f4207bece9'
 });
+
+Package.on_use(function (api, where) {
+  api.add_files('imap-server.js', 'server');
+});
